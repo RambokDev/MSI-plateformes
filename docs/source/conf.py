c1a1f7ac4750397c5b68d121bf436c11ef2e1b63
icam <charles.gauthereau@2024.icam.fr>
2023-11-21 14:42:38 +0100

    v1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.plantuml',
     'sphinx_ros',
 ]
 templates_path = ['_templates']
